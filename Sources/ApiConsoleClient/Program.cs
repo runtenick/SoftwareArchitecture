28211244095091807d2b1923bf6b56b5e61c780b
runtenick <runtenick2@gmail.com>
2023-02-13 21:31:45 +0100

    wrote console client (get route) but couldn t test

---
+++ Sources/ApiConsoleClient/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DTO;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace ConsoleClient
+{
+    class Porgram
+    {
+        static async Task Main()
+        {
+            Console.WriteLine("Start...");
+
+            using(var client = new HttpClient())
+            {
+                var result = await client.GetAsync("http://localhost:7175/api/Champions");
+                var champions = await result.Content.ReadFromJsonAsync<List<ChampionDto>>();
+
+                foreach(var champ in champions)
+                {
+                    Console.WriteLine(champ.Name);
+                }
+            }
+        }
+    }
+}
