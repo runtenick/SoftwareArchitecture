310acd829601abe0e19f6cbfeb02d7f93d45d625
runtenick <runtenick2@gmail.com>
2023-03-11 12:02:25 +0100

    added SkillMapper

---
+++ Sources/EF_Champions/Mapper/SkillMapper.cs
@@ -0,0 +1,18 @@
+﻿using EF_Champions.Entities;
+using Model;
+
+namespace EF_Champions.Mapper
+{
+    public static class SkillMapper
+    {
+        public static SkillEntity SkillToEntity(this Skill skill)
+        {
+            return new SkillEntity
+            {
+                Name = skill.Name,
+                Description = skill.Description,
+                SkillType = skill.Type
+            };
+        }
+    }
+}
