42c5a948432b6992dff4c9d65c2d749cfc4480e4
runtenick <runtenick2@gmail.com>
2023-03-11 07:43:35 +0100

    updated db + added SkillEntity

---
+++ Sources/EF_Champions/Entities/SkillEntity.cs
@@ -0,0 +1,20 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Champions.Entities
+{
+    public class SkillEntity
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public SkillType SkillType { get; set; }
+    }
+}
