0153e48b4c816e8db80ec57f41485c7f1834c384
runtenick <runtenick2@gmail.com>
2023-03-25 05:10:50 +0100

    HttpManager class done for champions :building_construction:

--- Sources/HttpManager/HttpManager.cs
+++ Sources/HttpManager/HttpManager.cs
@@ -1,6 +1,8 @@
 ﻿using Api.Pagination;
 using Model;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
 
 namespace HttpManager
 {
@@ -16,25 +18,63 @@ namespace HttpManager
 
         public async Task<Page> GetChampions(int index, int count)
         {
-            var result = await _httpClient.GetAsync($"api/Champions?index={index}&count={count}");
-            var page = await result.Content.ReadFromJsonAsync<Page>();
+            var response = await _httpClient.GetAsync($"api/Champions?index={index}&count={count}");
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error while getting champions, status code: {response.StatusCode}");
+            }
+            var page = await response.Content.ReadFromJsonAsync<Page>();
             return page;
         }   
 
         public async Task<Champion> GetChampionByName(string name)
         {
-            var result = await _httpClient.GetAsync($"api/Champions/{name}");
-            var champion = await result.Content.ReadFromJsonAsync<Champion>();
+            var response = await _httpClient.GetAsync($"api/Champions/{name}");
+            if(!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error while getting champion {name}, status code: {response.StatusCode}");
+            }
+            var champion = await response.Content.ReadFromJsonAsync<Champion>();
             return champion;
         }
 
         public async Task<Champion> PostChampion(Champion champion)
         {
-            var result = await _httpClient.GetAsync($"api/{champion}");
-            var champRes = await result.Content.ReadFromJsonAsync<Champion>();
-            return champion;
+            /* here we create a JsonContent object to serialize the champion
+             * to JSON for the HTTP request body */
+            var championContent = JsonContent.Create(champion);
+            var response = await _httpClient.GetAsync($"api/{champion}");
+            if(response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error while posting champion {champion.Name}, status code {response.StatusCode}");
+            }
+            var champRes = await response.Content.ReadFromJsonAsync<Champion>();
+            return champRes;
         }
 
-        //public async Task<Champion> PutChampion(Champion )
+        public async Task<Champion> PutChampion(Champion champion)
+        {
+            var json = JsonSerializer.Serialize(champion);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"api/Champions/{champion.Name}", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error while updating champion, status code: {response.StatusCode}");
+            }
+
+            var champRes = await response.Content.ReadFromJsonAsync<Champion>();
+            return champRes;
+        }
+
+        public async Task DeleteChampion(string name)
+        {
+            var response = await _httpClient.DeleteAsync($"api/Champions/{name}");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Error while deleting champion, status code: {response.StatusCode}");
+            }
+        }
     }
 }
